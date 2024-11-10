7265a0372ae8dedc7ed163ad4c4b6333c954569c
elmehdi-elfedini <100132930+elmehdi-elfedini@users.noreply.github.com>
2024-11-10 15:01:35 +0100

    Update urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,18 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
-from products.views import acceuil 
+from products.views import acceuil
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import handler404
 from products.views import page_404_view
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-handler404 = page_404_view # for 404 page
+# Custom 404 error handler
+handler404 = page_404_view 
 urlpatterns = [
-    path('vialys-admin/', admin.site.urls),
-    path('',acceuil, name='acceuil'),  
-    path('', include('products.urls')),  
-    path('', include('blog.urls')),  
+    # Admin panel route
+    path('vialys-admin/', admin.site.urls),    
+    path('', acceuil, name='acceuil'),      
+    path('products/', include('products.urls')),
+    path('', include('blog.urls')), 
     path('ckeditor/', include('ckeditor_uploader.urls')),
-]
-urlpatterns+=staticfiles_urlpatterns()
+] 
+
+# Add media files (images, uploaded files) serving URLs
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Automatically serve static files during development (Django does this for you in debug mode)
+urlpatterns += staticfiles_urlpatterns()
