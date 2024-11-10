b487b2cc9a03d0a74a315ae34c8a19bd19cf0b6f
elmehdi-elfedini <100132930+elmehdi-elfedini@users.noreply.github.com>
2024-11-10 16:34:42 +0100

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    'whitenoise.runserver_nostatic',  # Add this before 'django.contrib.staticfiles'
     "django.contrib.staticfiles",
     "ckeditor", # For the blog editor 
     "ckeditor_uploader", # For blog editor uploader
